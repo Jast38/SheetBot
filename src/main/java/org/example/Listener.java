1d603b938c4117965513e5a6a9bfd1c6b567303d
JannickSt <Jannick.Stuby@gmail.com>
2020-11-23 00:08:09 +0100

    edited Listener

--- src/main/java/org/example/Listener.java
+++ src/main/java/org/example/Listener.java
@@ -1,25 +1,30 @@
 package org.example;
 
+import java.awt.Color;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.*;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 //TODO: Iterate through COMMANDS, invoke corresponding Method when Command
 // recognized using  String Split and Reflection API
-//TODO: add argument to SheetParser to specify Week
+//TODO: add argument to SheetParser to specify Day
 public class Listener extends ListenerAdapter {
   private static final ArrayList<String> COMMANDS = new
       ArrayList<>(Arrays.asList(
       "help",
       "print",
       "admin"));
-  private static final String ADRIAN =
+  private static final String MEDTECH =
       "1dAEegeAXQArH_FXoSdETV8XsP4Dt7uAFaEt4-VpP0vY";
   private static final String BENNY =
       "1-xniicYlY5jNJiqv6qGSTVO40W_cpme9qZOorfQb1AE";
@@ -35,49 +40,80 @@ public class Listener extends ListenerAdapter {
    * @param event MessageReceivedEvent
    */
   @Override
-  public void onMessageReceived(final MessageReceivedEvent event) {
+  public void onGuildMessageReceived(final GuildMessageReceivedEvent event) {
     Message message = event.getMessage();
+    Guild guild = event.getGuild();
     String msg = message.getContentRaw();
     User author = event.getAuthor();
+    Member member = event.getMember();
 
     System.out.println("We received a message from "
         + author.getName() + ": "
         + message.getContentDisplay()
+        + " in guild "
+        + guild.getName()
     );
-    if (msg.startsWith("!sheet")) {
+    if (!msg.contains("```JAVA") && Objects.equals(member,
+        guild.getSelfMember())) {
+      deleteAfterDelay(message);
+    } else if (msg.startsWith("!sheet")) {
       if (msg.contains("help")) {
         reactToHelp(event);
       } else if (msg.contains("admin")) {
         reactToAdmin();
-      } else {
+      } else if (msg.contains("print")) {
         reactToSheet(event, author, msg);
       }
     }
   }
 
-  private void reactToSheet(final MessageReceivedEvent event, final User author,
+  private void deleteAfterDelay(Message message) {
+    TimerTask delete = new TimerTask() {
+      @Override
+      public void run() {
+        message.delete().queue();
+        System.out.println("Message deleted");
+      }
+    };
+
+    //delete after 2 minutes
+    LocalDateTime date = LocalDateTime.now().plusMinutes(2);
+    Date delay = Date.from(date.atZone(ZoneId.systemDefault()).toInstant());
+
+    Timer timer = new Timer();
+    timer.schedule(delete, 4000L);
+    System.out.println("Deletion scheduled for " + delay);
+  }
+
+  private void reactToSheet(final GuildMessageReceivedEvent event,
+                            final User author,
                             final String messageContentRaw) {
 
-    if (messageContentRaw.contains("print")) {
-      //TODO: add to database, check for Validity
-      SheetParser parser = new SheetParser(dataManager);
-      MessageBuilder toPrint = new MessageBuilder()
-          .append(author.getAsMention())
-          .append("\n\n");
-      try {
-        if (messageContentRaw.contains("Adrian")) {
-          toPrint.append(parser.output(ADRIAN).getContentRaw());
-        } else if (messageContentRaw.contains("Benny")) {
-          toPrint.append(parser.output(BENNY).getContentRaw());
-        } else {
-          toPrint.append("No SpreadsheetId for this name. Typo?");
-        }
-      } catch (GeneralSecurityException | IOException e) {
-        e.printStackTrace();
+    //TODO: add to database, check for Validity of Name, check for guild
+    // -> only allow if member of that guild added
+    // db: name(id)-spreadsheetId-guild-addedBy-addedDate
+    SheetParser parser = new SheetParser(dataManager);
+    MessageBuilder toPrint = new MessageBuilder()
+        .append(author.getAsMention())
+        .append("\n\n");
+    try {
+      if (org.apache.commons.lang3.StringUtils.containsIgnoreCase(
+          messageContentRaw, "MedTech")) {
+        toPrint.appendCodeBlock(parser.output(MEDTECH).getContentRaw(), "JSON");
+      } else if (org.apache.commons.lang3.StringUtils.containsIgnoreCase(
+          messageContentRaw, "Benny")) {
+        toPrint.appendCodeBlock(parser.output(BENNY).getContentRaw(), "JSON");
+      } else {
+        toPrint.append("No SpreadsheetId for this name. Typo?");
       }
-      event.getChannel().sendMessage(toPrint.build())
-          .queue();
+    } catch (GeneralSecurityException | IOException e) {
+      e.printStackTrace();
     }
+    if (messageContentRaw.contains("keep")) {
+      toPrint.replaceFirst("```JSON", "```JAVA");
+    }
+    event.getChannel().sendMessage(toPrint.build())
+        .queue();
   }
 
   //TODO: implement admin features: kick members, add remove spreadsheet
@@ -85,19 +121,19 @@ public class Listener extends ListenerAdapter {
   private void reactToAdmin() {
   }
 
-  private void reactToHelp(final MessageReceivedEvent event) {
-    MessageBuilder commandListBuilder = new MessageBuilder();
-    commandListBuilder.append("```Here is a list of my commands: \n\n")
-        .append("All commands must be preceded by '!sheet ' \n\n");
-
+  private void reactToHelp(final GuildMessageReceivedEvent event) {
+    Color lightBlue = new Color(69, 167, 242);
+    EmbedBuilder eb = new EmbedBuilder();
+    eb.setTitle("SheetBot Commands")
+        .setColor(lightBlue);
+    StringBuilder string = new StringBuilder();
     for (String command
         : COMMANDS) {
-      commandListBuilder.append(command).append("\n");
+      string.append("` !sheet ").append(command).append("`").append("\n\n");
     }
-    commandListBuilder.append("```");
+    eb.addField("",
+        string.toString(), false);
     event.getChannel()
-        .sendMessage(commandListBuilder.build()).queue();
+        .sendMessage(eb.build()).queue();
   }
-
 }
-
