562aa3df9707fbe21f9ba4adc2e7b2bfdf2f32dd
JannickSt <Jannick.Stuby@gmail.com>
2020-11-22 13:10:11 +0100

    added DataManager

--- src/main/java/org/example/DataManager.java
+++ src/main/java/org/example/DataManager.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 //TODO: implement database to save Spreadsheet IDs with corresponding names and
 // messages/logs
 public class DataManager {
+  public InputStream getFile(final String resourcePath) throws FileNotFoundException {
+    InputStream in = DataManager.class.getResourceAsStream(resourcePath);
+    if (in == null) {
+      throw new FileNotFoundException("File not found "
+      + resourcePath);
+    }
+    return null;
+  }
 }
 
