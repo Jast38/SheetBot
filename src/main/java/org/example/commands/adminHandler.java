f45a0b88c3df2c98954f621c61b5165e93f6e0eb
JannickSt <Jannick.Stuby@gmail.com>
2020-11-24 23:32:00 +0100

    added command folder structure and classes

---
+++ src/main/java/org/example/commands/adminHandler.java
@@ -0,0 +1,11 @@
+package org.example.commands;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+/**
+ * //TODO: add admin commands
+ * @author Jannick Stuby
+ * @version 11/24/20
+ */
+public class adminHandler extends ListenerAdapter {
+}
---
+++ src/main/java/org/example/commands/sheetAdd.java
@@ -0,0 +1,8 @@
+package org.example.commands;
+
+/**
+ * @author Jannick Stuby
+ * @version 11/24/20
+ */
+public class sheetAdd {
+}
