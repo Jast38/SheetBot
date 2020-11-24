c185106838f303ef27ffaa6816c17b3428ecbdc1
JannickSt <Jannick.Stuby@gmail.com>
2020-11-24 23:32:00 +0100

    added command folder structure and classes

---
+++ src/main/java/org/example/commands/sheetHandler.java
@@ -0,0 +1,10 @@
+package org.example.commands;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+/**
+ * @author Jannick Stuby
+ * @version 11/24/20
+ */
+public class sheetHandler extends ListenerAdapter {
+}
